0db823b238f5fd15f1570d323129e9053ca19977
Jayson Marwaha <jayson_marwaha@hms.harvard.edu>
2022-04-28 16:46:58 -0400

    Tried to get months to work

--- streamlit_app.KT.py
+++ streamlit_app.KT.py
@@ -30,28 +30,15 @@ print(subset.Month)
 #machines list: ['ILLUMINA', 'LS454', 'ABI_SOLID', 'ION_TORRENT', 'PACBIO_SMRT', 'OXFORD_NANOPORE', 'BGISEQ', 'CAPILLARY', 'HELICOS', 'COMPLETE_GENOMICS']
 #study type list: ['cDNA' 'ChIP' 'RANDOM' 'RANDOM PCR' 'unspecified' 'PCR' 'size fractionation' 'other' 'Restriction Digest' 'PolyA' 'Inverse rRNA''Oligo-dT' 'Hybrid Selection' 'RT-PCR' 'Reduced Representation''repeat fractionation' 'DNase' 'MBD2 protein methyl-CpG binding domain''MNase' 'MDA' 'RACE' 'padlock probes capture method''5-methylcytidine antibody' 'CAGE' 'ChIP-Seq']
 
-### P2.3 ###
-#countries = st.multiselect('Select Countries', pd.unique(df["Country"]), ['Austria', 'Germany','Iceland', 'Spain','Sweden', 'Thailand','Turkey'])
-#subset = subset[subset["Country"].isin(countries)]
-
-### P2.4 ###
-# replace with st.selectbox
-#cancer = st.selectbox('Select Cancer', pd.unique(df["Cancer"]))
-#subset = subset[subset["Cancer"] == cancer]
-
 #Visualization
 chart = alt.Chart(subset).mark_bar().encode(
     x=alt.X("Month:O"),
     y=alt.Y("sum_spots:Q"),
-    #color=alt.Color("Rate",scale=alt.Scale(type='log', domain=(0.01, 1000), clamp=True), legend=alt.Legend(title="Mortality rate per 100k")),
-    #tooltip=["Rate"],
 ).transform_aggregate(
     sum_spots='sum(Spots)',
     groupby=["Month"]
 ).properties(
     title="test plot"
-    #title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
 )
-### P2.5 ###
 
 st.altair_chart(chart, use_container_width=True)
